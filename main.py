26cf5877d7232ee1dd9330a58040d1811d36571a
nestorcalvo <nestor.calvo@udea.edu.co>
2023-12-19 11:44:39 -0500

    Main file changed for EC2

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import os
 
 app = FastAPI()
 
-GENERAL_PATH = './prueba/'
+GENERAL_PATH = '/efs/pronunciation_data'
 
 class Item(BaseModel):
     path: str
     id: str
+    sesion: str
 
 
 @app.get("/")
@@ -28,17 +29,18 @@ def health():
 def plot_audio_figure(item: Item):
     try:
         subject_path = os.path.join(GENERAL_PATH, item.id)
-        print(subject_path)
+        sesion_path = os.path.join(subject_path, item.sesion)
+        #print(subject_path)
         feature_name = str(item.id) + '.csv'
-        print(feature_name)
-        feature_storage_path = os.path.join(subject_path, feature_name)
-        print(feature_storage_path)
-        os.makedirs(subject_path, exist_ok=True) 
-        graph_creator = Graph(item.path, feature_storage_path, subject_path)
+        #print(feature_name)
+        feature_storage_path = os.path.join(sesion_path, feature_name)
+        image_storage_path = os.path.join(sesion_path, 'images')
+        #print(feature_storage_path)
+        #os.makedirs(subject_path, exist_ok=True)
+        graph_creator = Graph(item.path, feature_storage_path, image_storage_path)
         graph_success = graph_creator.plot_store_graph()
         if graph_success == False:
             raise HTTPException(status_code=404, detail="Problems with the graph creation, check the backend")
-        return item 
-        
+        return item
     except:
         raise HTTPException(status_code=404, detail="Item not found")
