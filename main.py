5a5bf499c18d1806a47b6d752dd96fae2c2839e9
nestor <nestor.calvo@udea.edu.co>
2024-01-17 13:06:15 -0500

    Change to form-data

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
-from pydantic import BaseModel
-from typing import Union
-from fastapi import FastAPI, HTTPException
-from phonet.graph import Graph
 import os
+from typing import Union
+
+from fastapi import FastAPI, HTTPException, Form
+from pydantic import BaseModel
+
+from phonet.graph import Graph
 
 app = FastAPI()
 
-GENERAL_PATH = '/efs/pronunciation_data'
+GENERAL_PATH = '/home/nestor/Documents/Maestria/Pronunciation/phonet/prueba'
+
 
 class Item(BaseModel):
     path: str
@@ -26,22 +29,25 @@ def health():
 
 
 @app.get("/audio_process")
-def plot_audio_figure(item: Item):
+def plot_audio_figure(path: str = Form(), id: str = Form(), sesion: str = Form()):
     try:
-        subject_path = os.path.join(GENERAL_PATH, item.id)
-        sesion_name = 'S'+ str(item.sesion)
+        subject_path = os.path.join(GENERAL_PATH, id)
+        sesion_name = 'S' + str(sesion)
         sesion_path = os.path.join(subject_path, sesion_name)
-        #print(subject_path)
-        feature_name = str(item.id) + '.csv'
-        #print(feature_name)
+        # print(subject_path)
+        feature_name = str(id) + '.csv'
+        # print(feature_name)
         feature_storage_path = os.path.join(sesion_path, feature_name)
         image_storage_path = os.path.join(sesion_path, 'images')
-        #print(feature_storage_path)
+        # print(feature_storage_path)
         os.makedirs(image_storage_path, exist_ok=True)
-        graph_creator = Graph(item.path, feature_storage_path, image_storage_path)
+        graph_creator = Graph(
+            path, feature_storage_path, image_storage_path)
         graph_success = graph_creator.plot_store_graph()
         if graph_success == False:
-            raise HTTPException(status_code=404, detail="Problems with the graph creation, check the backend")
-        return item
+            raise HTTPException(
+                status_code=404, detail="Problems with the graph creation, check the backend")
+        success = "Imagenes generadas exitosamente para el id "+str(id)+" fueron almacenadas en la sesion " + str(sesion)
+        return success
     except:
         raise HTTPException(status_code=404, detail="Item not found")
